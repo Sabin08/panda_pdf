a8e54ec426c610bd551f10c739b1ec8952544cb3
Sabin08 <metusab@gmail>
2025-01-18 14:25:04 -0500

    error fixed and uploads working fine

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import os
 from flask import Flask, request, render_template, send_file
 from werkzeug.utils import secure_filename
-# from pdf2docx import Converter
-import pdf2docx as p
+from pdf2docx import Converter
 import zipfile
 
 app = Flask(__name__)
@@ -12,31 +11,37 @@ app.config['UPLOAD_FOLDER'] = 'uploads'
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
+@app.route('/convert', methods=['POST'])
 def convert():
     if request.method == 'POST':
-        f = request.files['file']
+        # return request.files.getlist('file')
+        files = request.files.getlist('file')  # Get the list of uploaded files
+        # return files
         temp_folder = os.path.join(app.config['UPLOAD_FOLDER'], 'temp')
         os.makedirs(temp_folder, exist_ok=True)
+        
         docx_files = []
-        for pdf_file in pdf_file:
-            if pdf_file != '':
-                filename = secure_filename(pdf_file)
+
+        for pdf_file in files:
+            if pdf_file.filename != '':
+                filename = secure_filename(pdf_file.filename)
                 pdf_path = os.path.join(temp_folder, filename)
                 pdf_file.save(pdf_path)
-                docx_path = os.path.join(temp_folder, os.path.splitext(filename[0] + '.docx'))
-                converter = p.Converter(pdf_path)
+
+                docx_path = os.path.join(temp_folder, os.path.splitext(filename)[0] + '.docx')
+
+                converter = Converter(pdf_path)
                 converter.convert(docx_path, start=0, end=None)
                 converter.close()
+
                 docx_files.append(docx_path)
 
-    zip_file = os.path.join(app.config['UPLOAD_FOLDER'], 'converted.files.zip')
-    with zipfile.ZipFile(zip_file, 'w') as zip_file:
-        for docx_file in docx_files:
-            zip_file.write(docx_file,os.path.basename(docx_file))
-        return send_file(zip_file, as_attachment=True)
-                
+        zip_file_path = os.path.join(app.config['UPLOAD_FOLDER'], 'converted_files.zip')
+        with zipfile.ZipFile(zip_file_path, 'w') as zipf:
+            for docx_file in docx_files:
+                zipf.write(docx_file, os.path.basename(docx_file))
+
+        return send_file(zip_file_path, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
